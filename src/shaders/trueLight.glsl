b546b827fa9f7b6d4228a944cbe4f8e9bc4f3478
Bub <bub@bub.bub>
2024-08-08 13:01:32 -0500

    Shader rewrite, the shader looks the same and works on web!

--- src/shaders/trueLight.glsl
+++ src/shaders/trueLight.glsl
@@ -1,14 +1,13 @@
-// Circle cutout shader
+// Circle cutout shader with some cool 'blending'
 
+uniform float playerX;
+uniform float playerY;
+uniform float shaderTween;
+uniform float scale;
 
-extern number playerX;
-extern number playerY;
-extern number shaderTween;
-extern number scale;
-
-number radius = 220 * scale;
-vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ) {
-    number distance = pow(pow(screen_coords.x - playerX, 2) + pow(screen_coords.y - playerY, 2), shaderTween);
-    number alpha = distance / radius;
-    return vec4(0, 0, 0, alpha);
+vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
+    float radius = 220.0 * scale;
+    float distance = pow(pow(screen_coords.x - playerX, 2.0) + pow(screen_coords.y - playerY, 2.0), shaderTween);
+    float alpha = distance / radius;
+    return vec4(0.0, 0.0, 0.0, alpha);
 }
